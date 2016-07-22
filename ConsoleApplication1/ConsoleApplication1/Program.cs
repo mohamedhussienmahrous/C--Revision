688aee94fb5238b6a1440f2f39ebb3775d3d4c50
mohamedhussienmahrouse <mohamed_diab12317@outlook.com>
2016-07-22 13:46:24 -0700

    Read/write/DataTypes

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Video 2
+            //Console.Write("Please Enter Your First Name: ");
+            //string FirstName=Console.ReadLine();
+            //Console.Write("Please Enter your Last Name: ");
+            //string LastName = Console.ReadLine();
+            //Console.WriteLine(" Your Name Is {0} {1}",FirstName,LastName);
+        //Video 3
+            //bool b=true;
+            //Console.WriteLine(b);
+
+            //int i = 0;
+            //Console.WriteLine("Min value = {0}.",int.MinValue);
+            //Console.WriteLine("Max value = {0}.",int.MaxValue);
+
+            //double d = 1252.2555255;
+            //Console.WriteLine(d);
+
+            //Video 4
+
+            //string name = "\"Mohamed\"";
+            //Console.WriteLine(name);
+            //string w123 = "One\nTwo\nThree";
+            //Console.WriteLine(w123);
+
+            //string w123 = @"c:diabfsd\fff\asa\rdt\grggg";
+            //Console.WriteLine(w123);
+
+        }
+    }
+}
